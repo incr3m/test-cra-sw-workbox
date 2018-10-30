f72e0b56a0e1bc6ed2bb5bef316df2ec4f9633bf
anfa <anfa@splitmedialabs.com>
2018-10-30 22:09:52 +0800

    use PwaUpdateChecker

--- build.js
+++ build.js
@@ -25,31 +25,31 @@ const webpackConfigProd = require("react-scripts/config/webpack.config.prod");
 // );
 
 // webpackConfigProd.resolve.alias["@ant-design/icons"] = "purched-antd-icons";
-let generateSwIdx;
-webpackConfigProd.plugins.forEach((p, i) => {
-  if (p.constructor.name === "GenerateSW") {
-    generateSwIdx = i;
-  }
-});
+// let generateSwIdx;
+// webpackConfigProd.plugins.forEach((p, i) => {
+//   if (p.constructor.name === "GenerateSW") {
+//     generateSwIdx = i;
+//   }
+// });
 
-if (generateSwIdx) webpackConfigProd.plugins.splice(generateSwIdx, 1);
+// if (generateSwIdx) webpackConfigProd.plugins.splice(generateSwIdx, 1);
 
-webpackConfigProd.plugins.push(
-  new WorkboxWebpackPlugin.GenerateSW({
-    clientsClaim: true,
-    skipWaiting: true,
-    exclude: [/\.map$/, /asset-manifest\.json$/],
-    importWorkboxFrom: "cdn",
-    cacheId: "sw-app",
-    navigateFallback: "/index.html",
-    navigateFallbackBlacklist: [
-      // Exclude URLs starting with /_, as they're likely an API call
-      new RegExp("^/_"),
-      // Exclude URLs containing a dot, as they're likely a resource in
-      // public/ and not a SPA route
-      new RegExp("/[^/]+\\.[^/]+$")
-    ],
-  })
-);
+// webpackConfigProd.plugins.push(
+//   new WorkboxWebpackPlugin.GenerateSW({
+//     clientsClaim: true,
+//     skipWaiting: true,
+//     exclude: [/\.map$/, /asset-manifest\.json$/],
+//     importWorkboxFrom: "cdn",
+//     cacheId: "sw-app",
+//     navigateFallback: "/index.html",
+//     navigateFallbackBlacklist: [
+//       // Exclude URLs starting with /_, as they're likely an API call
+//       new RegExp("^/_"),
+//       // Exclude URLs containing a dot, as they're likely a resource in
+//       // public/ and not a SPA route
+//       new RegExp("/[^/]+\\.[^/]+$")
+//     ],
+//   })
+// );
 
 require("react-scripts/scripts/build");
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Router, Link } from "@reach/router";
@@ -10,24 +10,78 @@ import nanoid from "nanoid";
 // export const RA = loadable(() => import("./test-routes/RouteA"));
 // export const RB = loadable(() => import("./test-routes/RouteB"));
 
-fetch("/index.html").then(d => {
-  d.json().then(v => {
-    console.log(v);
+// fetch("/index.html").then(d => {
+//   d.json().then(v => {
+//     console.log(v);
+//   });
+// });
+
+// caches.keys().then(function(cacheNames) {
+//   cacheNames.forEach(function(cacheName) {
+//     console.log(`updating index cache for ${cacheName}`);
+//     caches.open(cacheName).then(function(cache) {
+//       fetch("/?t=" + nanoid()).then(function(response) {
+//         cache.put("/index.html", response);
+//       });
+//     });
+//   });
+// });
+
+// fetch("/index.html?hfdsfh=werwsdfmkl")
+//   .then(function(response) {
+//     return response.text();
+//   })
+//   .then(function(myJson) {
+//     console.log(myJson);
+//   });
+
+function usePwaUpdateChecker(props) {
+  let [updated, setUpdated] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  useEffect(async () => {
+    if (checked) return;
+    try {
+      const serverRequest = "/?t=" + nanoid();
+      const [serverRoot, clientRoot] = await Promise.all([
+        fetch(serverRequest),
+        fetch("/index.html")
+      ]);
+      if (!serverRoot || serverRoot.status !== 200)
+        throw new Error("Error could not connect to server");
+      const [serverHtml, clientHtml] = await Promise.all([
+        serverRoot.text(),
+        clientRoot.text()
+      ]);
+      if (serverHtml === clientHtml) {
+        console.log("no updated needed");
+        setChecked(true);
+        return;
+      }
+      console.log("eupdating server caches...");
+      const cacheNames = await caches.keys();
+      for (const cacheName of cacheNames) {
+        console.log(`updating index cache for ${cacheName}`);
+        const cache = await caches.open(cacheName);
+        const newRoot = await fetch(serverRequest);
+        cache.put("/index.html", newRoot.clone());
+      }
+      setChecked(true);
+      setUpdated(true);
+    } catch (err) {
+      console.log("Error occurred while checking for updates..");
+      console.error(err);
+      setChecked(true);
+    }
   });
-});
-fetch("/?hfdsfh=werwsdfmkl")
-  .then(function(response) {
-    caches.open("sw-app-precache-https://f1cb2653.ngrok.io/").then(function(cache) {
-      cache.put("/", response);
-    });
-  })
-fetch("/index.html?hfdsfh=werwsdfmkl")
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-  });
+
+  return { updated };
+}
+
+function PwaStatus() {
+  const { updated } = usePwaUpdateChecker();
+  return <div>{updated ? "Updated!!!!!" : "qweww :("}</div>;
+}
 
 class Deflt extends Component {
   componentDidMount() {
@@ -55,9 +109,10 @@ class Deflt extends Component {
           test
         </button>
         <center>
-          <div style={{ maxWidth: 300 }}>
+          <PwaStatus />
+          {/* <div style={{ maxWidth: 300 }}>
             <p style={{ whiteSpace: "pre-wrap" }}>{longData}</p>
-          </div>
+          </div> */}
         </center>
       </div>
     );
